e667a8d90aaf566d4a54e421acfb8fa21f2348a3
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2023-02-22 17:08:14 -0500

    Initialize 1.12.2

---
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.example.examplemod;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(modid = ExampleMod.MODID,name = "Example Mod",version = "@VERSION@")
+public class ExampleMod {
+    public static final String MODID = "examplemod";
+}
